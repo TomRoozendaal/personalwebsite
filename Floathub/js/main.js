a17036829121a443ccff50386ba17471fa19b401
Smudgy <tmrgast@live.nl>
2017-12-05 23:54:50 +0100

    -

--- Floathub/js/main.js
+++ Floathub/js/main.js
@@ -1,4 +1,3 @@
-<script>
 // Get the modal
 var modal = document.getElementById('id01');
 
@@ -8,4 +7,48 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
-</script>
+
+$(document).ready(() => {
+  $('.blur').addClass("antiblur");
+
+  // $('.hide-button').on('click', () => {
+  //   $('.first-image').hide();
+  // });
+  //
+  // $('.show-button').on('click', () => {
+  //   $('.first-image').show();
+  // });
+  //
+  // $('.toggle-button').on('click', () => {
+  //   $('.first-image').toggle();
+  // });
+  //
+  // $('.fade-out-button').on('click', () => {
+  //   $('.fade-image').fadeOut(500);
+  // });
+  //
+  // $('.fade-in-button').on('click', () => {
+  //   $('.fade-image').fadeIn(4000);
+  // });
+  //
+  // $('.fade-toggle-button').on('click', () => {
+  //   $('.fade-image').fadeToggle();
+  // });
+  //
+  // $('.up-button').on('click', () => {
+  //
+  //   $('.slide-image').slideDown('slow');
+  //   $('.slide-image').hide();
+  //   $('.slide-image').slideUp(100);
+  //
+  // });
+  //
+  // $('.down-button').on('click', () => {
+  //   $('.slide-image').slideDown('slow');
+  // });
+  //
+  // $('.slide-toggle-button').on('click', () => {
+  //   $('.slide-image').slideToggle();
+  // });
+
+});
